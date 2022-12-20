94e31cb3f1e3c3d835267f1008f72d6bfd95e362
banta <banta.bernard@gmail.com>
2022-12-20 20:47:38 +0300

    Configure daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    content: [
+        './app/views/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/assets/stylesheets/**/*.css',
+        './app/javascript/**/*.js'
+    ],
+    daisyui: {
+        themes: [{
+            mytheme: {
+                "primary": "#D82028",
+                "secondary": "#F000B8",
+                "accent": "#37CDBE",
+                "neutral": "#3D4451",
+                "base-100": "#FFFFFF",
+                "info": "#3ABFF8",
+                "success": "#36D399",
+                "warning": "#FBBD23",
+                "error": "#F87272",
+            },
+        },],
+    },
+    plugins: [require("daisyui")],
 }
