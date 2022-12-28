8c68543bbd263c742f728afbf16d57307716fdcf
banta <banta.bernard@gmail.com>
2022-12-28 16:40:14 +0300

    Broadcast created countries

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,12 @@
 class Country < ApplicationRecord
+  # Callbacks
+  after_create_commit :update_countries
+
+  # Validations
   validates :name, presence: true
+
+  def update_countries
+    broadcast_prepend_to 'countries', partial: 'countries/new_country', locals: { country: self },
+                         target: 'countries'
+  end
 end
