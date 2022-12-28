1af8380efe402b137bfd04bb1bdd0728e12fad88
banta <banta.bernard@gmail.com>
2022-12-28 17:25:17 +0300

    Add action cable broadcast for update and deletion

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,13 @@
 class Country < ApplicationRecord
   # Callbacks
-  after_create_commit :update_countries
+  after_create_commit -> {
+    broadcast_prepend_to 'countries', partial: 'countries/new_country', locals: { country: self },
+                         target: 'countries' }
+
+  after_update_commit -> { broadcast_update_to 'countries', partial: 'countries/new_country',
+                                               locals: { country: self }, target: self }
+
+  after_destroy_commit -> { broadcast_remove_to 'countries', target: self }
 
   # Validations
   validates :name, presence: true
