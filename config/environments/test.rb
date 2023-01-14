448699e5e79e43b6e94ec5763c0a102a1173a6d4
banta <banta.bernard@gmail.com>
2023-01-14 21:22:24 +0300

    Suppress logger output for asset requests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
 end
