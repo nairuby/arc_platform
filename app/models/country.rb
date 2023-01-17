ba5638f7a7798877e3f5eb1d2460551d59f69e1f
banta <banta.bernard@gmail.com>
2023-01-17 04:47:30 +0300

    Fix the issue of creating countries

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,10 +1,10 @@
 class Country < ApplicationRecord
   # Callbacks
   after_create_commit -> {
-    broadcast_prepend_to 'countries', partial: 'countries/new_country', locals: { country: self },
-                         target: 'countries' }
+    broadcast_prepend_to 'countries', partial: 'countries/created_country',
+                         locals: { country: self }, target: 'countries' }
 
-  after_update_commit -> { broadcast_update_to 'countries', partial: 'countries/new_country',
+  after_update_commit -> { broadcast_update_to 'countries', partial: 'countries/updated_country',
                                                locals: { country: self }, target: self }
 
   after_destroy_commit -> { broadcast_remove_to 'countries', target: self }
