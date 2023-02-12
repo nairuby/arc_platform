d720130f91dc366974850bcabbd8c7cb04d527b4
banta <banta.bernard@gmail.com>
2023-02-12 18:43:23 +0300

    Add turbo frames after_create callback

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,6 +1,12 @@
 class Chapter < ApplicationRecord
   # Associations
   belongs_to :country
+
+  # Callbacks
+  after_create_commit -> {
+    broadcast_prepend_to 'chapters', partial: 'chapters/created_chapter',
+                         locals: { chapter: self }, target: 'chapters' }
+
   # Validations
   validates :name, :location, :country_id, :description, presence: true
   validates :name, uniqueness: true
