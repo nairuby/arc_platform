a11c4784189e3ec1922607254e8ada10f914fc3d
banta <banta.bernard@gmail.com>
2023-02-12 21:12:16 +0300

    Make it more dynamic

--- app/javascript/controllers/turbo_frames_controller.js
+++ app/javascript/controllers/turbo_frames_controller.js
@@ -2,13 +2,13 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
     connect() {
-        if (this.element.id == "new-chapter-modal") {
-            window.location.href = "#new-chapter-modal"
+        if (this.element.id.includes("chapter-modal")) {
+            window.location.href = "#" + this.element.id
         }
     }
 
     disconnect() {
-        if (this.element.id == "new-chapter-modal") {
+        if (this.element.id.includes("chapter-modal")) {
             window.location.href = "#"
         }
     }
