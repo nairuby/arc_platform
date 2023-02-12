ab0cabeffd6dad65793698da4aec31a4284a6ce8
banta <banta.bernard@gmail.com>
2023-02-12 18:41:03 +0300

    Add Chapter validations

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,7 @@
 class Chapter < ApplicationRecord
   # Associations
   belongs_to :country
+  # Validations
+  validates :name, :location, :country_id, :description, presence: true
+  validates :name, uniqueness: true
 end
