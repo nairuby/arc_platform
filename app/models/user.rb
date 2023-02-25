6124cb44cc4a1a013232aed78a4982aefb3282f5
Bernard Banta <banta.bernard@gmail.com>
2023-02-26 00:49:43 +0300

    Add callback method to set model defaults

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, 
          :confirmable, :lockable, :timeoutable
 
+  before_create :set_defaults # Set model defaults before create
+
   # Enums
   enum role: %i[member chapter_admin organization_admin]
+
+  private
+
+
+  ##
+  # A method to set model defaults if they are not set. e.g. if role is not set the default will be
+  # :member.
+  def set_defaults
+    self.role ||= :member
+  end
 end
