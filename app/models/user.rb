e2247bcc901977a22ec0f2339bf4cab0007927a9
Bernard Banta <banta.bernard@gmail.com>
2023-02-26 00:43:02 +0300

    Set 3 user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :confirmable, :lockable, :timeoutable
+
+  # Enums
+  enum role: %i[member chapter_admin organization_admin]
 end
