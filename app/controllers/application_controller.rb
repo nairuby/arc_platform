8b0297290199a2f0f509a972f18e52992ac87d5f
Bernard Banta <banta.bernard@gmail.com>
2023-02-26 03:47:40 +0300

    Show a flash message if use is not allowed to do an action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! # All users should be authenticated in all controllers by default
+
+  ##
+  # CanCanCan permission exception capture
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      if user_signed_in?
+        format.html { redirect_to root_path, alert: exception.message }
+      else
+        format.html { redirect_to root_path, alert: 'Please sign in to perform the action' }
+      end
+    end
+  end
 end
