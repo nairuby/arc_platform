d849f1f85a34a1a4b14067b12e36515bf75c4122
Bernard Banta <banta.bernard@gmail.com>
2023-02-26 20:59:03 +0300

    Update chapter id if user was saved

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # Devise override Registration create action
   def create
     super do
-      resource.users_chapters.create(chapter_id: params[:chapter_id], main_chapter: true)
+      if resource.persisted?
+        resource.users_chapters.create(chapter_id: params[:chapter_id], main_chapter: true)
+      end
     end
   end
 end
