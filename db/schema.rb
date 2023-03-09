fd1335ffd83211485dd0b62f6e2a0a197d7edef4
Bernard Banta <banta.bernard@gmail.com>
2023-03-09 19:14:36 +0300

    Add feature flags model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_235848) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_154521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_235848) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "feature_flags", force: :cascade do |t|
+    t.string "feature"
+    t.text "description"
+    t.boolean "enabled"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
