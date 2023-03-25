4524e31cfb34d7ae201915e0011aebadef792570
Bernard Banta <banta.bernard@gmail.com>
2023-03-25 13:29:15 +0300

    Remove typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://rubycommunity.africa' }
   config.action_mailer.delivery_method = :sendgrid_actionmailer
   config.action_mailer.sendgrid_actionmailer_settings = {
-    api_key: ERails.application.credentials.sendgrid_api_key,
+    api_key: Rails.application.credentials.sendgrid_api_key,
     raise_delivery_errors: true
   }
 end
