d592918fbfd4a94f2303172f8723e16acedecf70
Bernard Banta <banta.bernard@gmail.com>
2023-04-28 07:22:20 +0300

    Add a learning page with materials

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,9 +1,13 @@
 class LandingController < ApplicationController
   # People should not require authentication for following actions
-  skip_before_action :authenticate_user!, only: %i[index about]
+  skip_before_action :authenticate_user!, only: %i[index about learn]
   def index;end
 
   ##
   # About us page
   def about;end
+
+  ##
+  # Featured learning materials
+  def learn;end
 end
