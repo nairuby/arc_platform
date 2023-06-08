adc3800d36df47df932452fc151b37b4eb8f5502
JudahSan <baraka.judah@yahoo.com>
2023-06-08 16:27:17 +0300

    TODO: upload image, fix hidden submit button

--- app/models/learning_material.rb
+++ app/models/learning_material.rb
@@ -1,2 +1,7 @@
 class LearningMaterial < ApplicationRecord
+  has_one_attached :image
+  validates :title, presence: true
+  validates :difficulty, presence: true
+  # validates :image_url, presence: true 
+  validates :resource_url, presence: true
 end
