cd676e69a60d733a320555b55b4de553d846a804
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-04-16 11:29:52 +0300

    redesign hero section right grid with images and stat cards,  set up class methods for conference

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -12,4 +12,48 @@
 #  updated_at :datetime         not null
 #
 class Conference < ApplicationRecord
+  # Validations
+  validates :title, presence: true
+  validate :only_one_upcoming_and_ongoing_conference
+  validate :only_one_conference_per_year
+
+  # Enums
+  enum status: { upcoming: 0, ongoing: 1, past: 2 }
+
+  # Class methods
+
+  def self.current
+    where(status: %i[upcoming ongoing]).first
+  end
+
+  # Instance methods
+
+  def upcoming?
+    status == 'upcoming'
+  end
+
+  def ongoing?
+    status == 'ongoing'
+  end
+
+  def past?
+    status == 'past'
+  end
+
+  # Private methods
+
+  private
+
+  def only_one_upcoming_and_ongoing_conference
+    return if Conference.where(status: %i[upcoming ongoing]).count <= 1
+
+    errors.add(:status, 'Only one conference can be upcoming or ongoing')
+  end
+
+  def only_one_conference_per_year
+    return if Conference.where('extract(year from start_date) = ?', start_date.year).count <= 1
+
+    errors.add(:start_date, 'Only one conference can be held per year')
+  end
+
 end
