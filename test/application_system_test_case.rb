332017907c2cfa52b6ce11f30d6fad89f941e714
judahsan <baraka.judah@gmail.com>
2025-03-28 16:12:59 +0300

    fix linter errors

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,14 +3,14 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  if ENV["CAPYBARA_SERVER_PORT"]
-    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+  if ENV['CAPYBARA_SERVER_PORT']
+    served_by host: 'rails-app', port: ENV['CAPYBARA_SERVER_PORT']
 
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
       browser: :remote,
-      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
+      url: "http://#{ENV.fetch('SELENIUM_HOST', nil)}:4444"
     }
   else
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 end
